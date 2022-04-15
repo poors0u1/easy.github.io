c7603e75e7c4c0fbcd2546102c216da59e81fabf
poors0u1 <103748725+poors0u1@users.noreply.github.com>
2022-04-15 18:01:21 +0600

    Add files via upload

---
+++ first.js
@@ -0,0 +1,25 @@
+function check()
+{
+    var inp = document.getElementsByName('radio');
+    for (var i = 0; i < inp.length; i++) {
+        if (inp[i].type == "radio" && inp[i].checked) {
+            if(inp[i].value == "three") {
+                document.querySelector('.text').innerHTML = 'Ты ответил правильно! Молодец!';
+                document.getElementById("text").style.color = "green";
+            }
+            else {
+                document.querySelector('.text').innerHTML = 'Ты ответил неправильно. Правильный ответ: жол!';
+                document.getElementById("text").style.color = "red";
+
+        }
+    }
+}
+};
+
+function vis(){
+    document.getElementById("btn2").disabled = false;
+    document.getElementById("btn1").disabled = true;
+    document.getElementById("first").disabled = true;
+    document.getElementById("second").disabled = true;
+    document.getElementById("third").disabled = true;
+};
